009dd634b7cf935b896399b75b9c4be79b8a30b1
redblacktech <thecurrentsolution@gmail.com>
2024-01-15 14:28:47 +0000

    update

--- initialize.js
+++ initialize.js
@@ -4,21 +4,18 @@ document.addEventListener('DOMContentLoaded', function() {
   M.Tabs.init(tabs);
   document.getElementById('home-tab').style['display'] = 'block'
 
+  var slides = document.querySelectorAll('.mySlides');
+
   // Image Slideshow
-  var slideIndex = 1;
+  var slideIndex = 0;
   var intervalId;
 
   function showSlides() {
-      var slides = document.querySelectorAll('.mySlides');
       var slidesWrapper = document.querySelector('.slides-wrapper');
       var progressBar = document.querySelector('.progress-bar');
 
-      if (slideIndex > slides.length) {
-          slideIndex = 1;
-      }
-
-      slidesWrapper.style.transform = 'translateX(' + -100 * (slideIndex - 1) + '%)';
-      progressBar.style.width = (100 / slides.length) * slideIndex + '%';
+      slidesWrapper.style.transform = 'translateX(' + -100 * slideIndex + '%)';
+      progressBar.style.width = (100 / slides.length) * (slideIndex + 1) + '%';
   }
 
   // Set initial progress bar width
@@ -27,7 +24,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
   function startInterval() {
       intervalId = setInterval(() => {
-          slideIndex += 1;
+          slideIndex = (slideIndex + 1) % slides.length;
           showSlides();
       }, 5000); // Change slide every 5 seconds
   }
@@ -36,8 +33,35 @@ document.addEventListener('DOMContentLoaded', function() {
 
   window.plusSlides = function(n) {
       clearInterval(intervalId); // Clear interval before changing slide
-      slideIndex += n;
+      slideIndex = (slideIndex + n) % slides.length;
       showSlides();
       startInterval(); // Restart interval after changing slide
   };
+
+
+  /* Functions related to sliding */
+  function isPastCenter(element) {
+    const rect = element.getBoundingClientRect();
+    const centerY = window.innerHeight * 2 / 3;
+    console.log(rect.top, centerY)
+    // return rect.top < centerY
+    return rect.top < centerY
+  }
+
+  function handleScroll(elementId) {
+    const elem = document.getElementById(elementId);
+    console.log(isPastCenter(elem))
+    if (isPastCenter(elem)) {
+      elem.classList.add('active');
+      window.removeEventListener('scroll', handleScroll); // Remove the event listener once animation is triggered
+    }
+  }
+
+  const hardwoodBackground = 'working-with-us'
+
+  window.addEventListener('scroll', () => handleScroll(hardwoodBackground));
+
+  // Trigger the initial check in case the element is already in the center on page load
+  // handleScroll(hardwoodBackground);
+
 });
