a5061c00cc2b2b7a17985ca188afebdf07ad1db8
redblacktech <thecurrentsolution@gmail.com>
2024-01-21 15:15:01 +0000

    update

--- initialize.js
+++ initialize.js
@@ -1,21 +1,57 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   // Initialize tabs
-  var tabs = document.querySelector('.tabs');
-  M.Tabs.init(tabs);
-  document.getElementById('home-tab').style['display'] = 'block'
 
-  var slides = document.querySelectorAll('.mySlides');
+  const tabs = M.Tabs.init(document.querySelector('.tabs'));
+
+  function getTabFromHash() {
+    let tabId = window.location.hash;
+    if (tabId.startsWith('#') && tabId.endsWith('-tab')) {
+      return tabId.slice(1)
+    } else {
+      return null
+    }
+  }
+  window.setTab = tabId => {
+    tabs.select(tabId)
+  }
+
+  let tabId = getTabFromHash()
+  if (tabId) {
+    tabs.select(tabId)
+  } else {
+    // if no tab selected, make sure the home tab is visible (without actually scrolling to it)
+    document.getElementById('home-tab').style['display'] = 'block'
+  }
+
+  // initialize selects
+  let selectObject = M.FormSelect.init(document.querySelectorAll('select'));
+  window.jumpToContactAnchor = function () {
+    const targetElement = document.getElementById('contact-section');
+    setTimeout(() => {
+      targetElement.scrollIntoView({ behavior: 'smooth' })
+    }, 100)
+  }
+  window.setContactType = type => {
+    setTab('contact-tab')
+    const selectElement = document.getElementById('contact-select')
+    const optionIndex = Array.from(selectElement.options).findIndex(option => option.value === type);
+    const options = selectElement.querySelectorAll('option')
+    options.forEach(option => { option.selected = false })
+    options[optionIndex].selected = true
+    selectObject = M.FormSelect.init(selectElement)
+  }
 
   // Image Slideshow
+  var slides = document.querySelectorAll('.mySlides');
   var slideIndex = 0;
   var intervalId;
 
   function showSlides() {
-      var slidesWrapper = document.querySelector('.slides-wrapper');
-      var progressBar = document.querySelector('.progress-bar');
+    var slidesWrapper = document.querySelector('.slides-wrapper');
+    var progressBar = document.querySelector('.progress-bar');
 
-      slidesWrapper.style.transform = 'translateX(' + -100 * slideIndex + '%)';
-      progressBar.style.width = (100 / slides.length) * (slideIndex + 1) + '%';
+    slidesWrapper.style.transform = 'translateX(' + -100 * slideIndex + '%)';
+    progressBar.style.width = (100 / slides.length) * (slideIndex + 1) + '%';
   }
 
   // Set initial progress bar width
@@ -23,19 +59,22 @@ document.addEventListener('DOMContentLoaded', function() {
   progressBar.style.width = (100 / document.querySelectorAll('.mySlides').length) + '%';
 
   function startInterval() {
-      intervalId = setInterval(() => {
-          slideIndex = (slideIndex + 1) % slides.length;
-          showSlides();
-      }, 5000); // Change slide every 5 seconds
+    intervalId = setInterval(() => {
+      slideIndex = (slideIndex + 1) % slides.length;
+      showSlides();
+    }, 5000); // Change slide every 5 seconds
   }
 
   startInterval();
 
-  window.plusSlides = function(n) {
-      clearInterval(intervalId); // Clear interval before changing slide
-      slideIndex = (slideIndex + n) % slides.length;
-      showSlides();
-      startInterval(); // Restart interval after changing slide
+  window.plusSlides = function (n) {
+    clearInterval(intervalId); // Clear interval before changing slide
+    slideIndex = (slideIndex + n) % slides.length;
+    if (slideIndex < 0) {
+      slideIndex = slides.length - 1
+    }
+    showSlides();
+    startInterval(); // Restart interval after changing slide
   };
 
 
@@ -43,14 +82,12 @@ document.addEventListener('DOMContentLoaded', function() {
   function isPastCenter(element) {
     const rect = element.getBoundingClientRect();
     const centerY = window.innerHeight * 2 / 3;
-    console.log(rect.top, centerY)
     // return rect.top < centerY
     return rect.top < centerY
   }
 
   function handleScroll(elementId) {
     const elem = document.getElementById(elementId);
-    console.log(isPastCenter(elem))
     if (isPastCenter(elem)) {
       elem.classList.add('active');
       window.removeEventListener('scroll', handleScroll); // Remove the event listener once animation is triggered
@@ -60,8 +97,5 @@ document.addEventListener('DOMContentLoaded', function() {
   const hardwoodBackground = 'working-with-us'
 
   window.addEventListener('scroll', () => handleScroll(hardwoodBackground));
-
-  // Trigger the initial check in case the element is already in the center on page load
-  // handleScroll(hardwoodBackground);
 
 });
